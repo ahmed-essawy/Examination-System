62dfceabd1fc33b290340fc6886b60817af72249
wafaa <fofa99441@yahoo.com>
2017-02-26 20:01:18 +0200

    Build Courses & Topics Layer

---
+++ App_Code/TopicsLayer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Topics
+/// </summary>
+public class TopicsLayer
+{
+    private static DataSet ds = new DataSet();
+    public static DataSet SelectTopics()
+    {
+        string str = "SELECT TP_ID,TP_Name,t.CR_ID,CR_Name FROM Topics t,Courses c where c.CR_ID=t.CR_ID";
+        ds = DAL.RunSelect(str);
+        return ds;
+    }
+    public int InsertTopic(string TP_Name, int CR_ID)
+    {
+        string str = "[Insert_Topic]";
+        SqlParameter param1 = new SqlParameter("@TP_Name", TP_Name);
+        SqlParameter param2 = new SqlParameter("@CR_ID", CR_ID);
+        int affected = DAL.RunDML(str, new SqlParameter[] { param1, param2 });
+        return affected;
+    }
+
+    public int UpdateTopic(int TP_ID, string TP_Name, string CR_Name, int CR_ID)
+    {
+        string str = "[Update_Topic]";
+        SqlParameter param1 = new SqlParameter("@TP_ID", TP_ID);
+        SqlParameter param2 = new SqlParameter("@TP_Name", TP_Name);
+        SqlParameter param3 = new SqlParameter("@CR_ID", CR_ID);
+        int affected = DAL.RunDML(str, new SqlParameter[] { param1, param2, param3 });
+        return affected;
+    }
+
+    public int DeleteTopic(int TP_ID,int CR_ID)
+    {
+        string str = "Delete_Topic";
+        SqlParameter param2 = new SqlParameter("@TP_ID ", TP_ID);
+        SqlParameter param1 = new SqlParameter("@CR_ID ", CR_ID);
+        int affected = DAL.RunDML(str, new SqlParameter[] { param1,param2 });
+        return affected;
+    }
+}
