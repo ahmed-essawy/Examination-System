363673bb107afc40eb417be45d0ab97cf1fd419a
Hebatallah <HebatallahMansour22@gmail.com>
2017-03-01 16:29:31 +0200

    Added Display Specific Student And display Stydent Courses

---
+++ CPanel/Students/DisplayStudentCourses.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CPanel_DisplayStudentCourses : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        /*
+        int id = int.Parse(TextBox1.Text);
+        StudentsLayer.DisplayStudentName(id);
+        GridView1.Caption ="Student Name: "+ StudentsLayer.ds.ToString();*/
+    }
+}
