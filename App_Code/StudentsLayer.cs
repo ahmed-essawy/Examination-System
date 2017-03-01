2f1347da8a5e648c8d0a83ab012db5c258944ce9
Hebatallah <HebatallahMansour22@gmail.com>
2017-03-01 11:01:47 +0200

    Built Students BL

--- App_Code/StudentsLayer.cs
+++ App_Code/StudentsLayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,146 @@ using System.Web;
 /// </summary>
 public class StudentsLayer
 {
-    private static DataSet ds = new DataSet();
+   public static DataSet ds = new DataSet();
 
-    public static DataSet SelectAllStudents()
+    public static DataSet SelectAllStudents()  
     {
-        string str = "SELECT * FROM Students";
+        string str = "SELECT a.ST_ID,a.ST_Fname,a.ST_Lname,a.ST_Name,a.ST_Age,a.ST_Phone,a.ST_Salary,a.ST_Street,a.ST_City,a.ST_Country,a.ST_JoinDate,b.DP_Name FROM Students a,Departments b where a.DP_ID=b.DP_ID";
         ds = DAL.RunSelect(str);
         return ds;
     }
+
+    public static DataSet SelectStudentById(int ST_ID)
+    {
+        string str = "select a.*,b.DP_Name from Students a,Departments b where a.DP_ID=b.DP_ID and a.ST_ID=" + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+
+    public static DataSet SelectDP()
+    {
+        string str = "select DP_Name,DP_ID from Departments ";
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+
+    public static DataSet DisplayStudentName(int ST_ID)
+    {
+        string str = "SELECT ST_Name from Students where ST_id=" + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+
+
+    public static DataSet DisplayStudentCourses(int ST_ID)
+    {
+        string str = "select a.CR_Name from Courses a,IN_ST_CR c where  a.CR_ID=c.CR_ID and c.ST_ID= " + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+
+    public static DataSet Display_EX_ST_ANS_CorrAns(int ST_ID)
+    {
+        string str = "select a.EX_ID,a.Answer,b.QS_Correct,c.ST_Name   from ST_EX_Answers a,Questions b,Students c   where a.QS_ID=b.QS_ID and a.ST_ID=c.ST_ID and c.ST_ID=" + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+    }
+
+    public static DataSet DisplayResults(int ST_ID)
+    {
+        string str = "select a.ST_Name,b.CR_Name,b.CR_Grade,c.Grade   from Students a,Courses b,IN_ST_CR c where  a.ST_ID=C.ST_ID and b.CR_ID=C.CR_ID and c.ST_ID=" + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+    }
+
+    public static DataSet DisplayStudentExams(int ST_ID)
+    {
+        string str = "select distinct a.ST_Name,b.EX_ID,c.EX_Date from Students a,ST_EX_Answers b,Exams c where a.ST_ID=b.ST_ID and b.EX_ID=c.EX_ID and b.ST_ID=" + ST_ID;
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+    public static DataSet DisplayExamByDate(string Date)
+    {
+        string str = "select a.ST_Name,convert(varchar(10),b.[Date]) [Date],b.ST_ID   from Students a,ST_EX_Answers b where a.ST_ID=b.ST_ID and b.[Date]='" + Date+"'";
+        ds = DAL.RunSelect(str);
+        return ds;
+
+    }
+    public static DataSet DisplayAllExamsDates()
+    {
+        string str = "select convert(varchar(10),Date)[Date] from ST_EX_Answers";
+        ds = DAL.RunSelect(str);
+        return ds;
+    }
+    public static DataSet DisplayCorrectiveByEX_ID(int EX_ID)
+    {
+        SqlParameter par1 = new SqlParameter("@EX_ID", EX_ID);
+        ds = DAL.RunSelect("Check_Corrective",new[] { par1 });
+        return ds;
+    }
+
+    public static DataSet DisplayEX_IDs()
+    {
+        string str = "select EX_ID from Exams";
+        ds = DAL.RunSelect(str);
+        return ds;
+    }
+
+    public static DataSet DisplaycorrectiveByDept(int DP_ID)
+    {
+        SqlParameter par1 = new SqlParameter("@DP_ID", DP_ID);
+        ds = DAL.RunSelect("Check_Corrective_By_Dep", new[] { par1 });
+        return ds;
+    }
+
+
+    public static int InsertStudent(int ST_ID, string ST_Fname, string ST_Lname,string ST_Bdate,string ST_Phone,decimal ST_Salary,string ST_Street,string ST_City,string ST_Country,int DP_ID)
+    {
+
+        SqlParameter par1 = new SqlParameter("@ST_ID", ST_ID);
+        SqlParameter par2 = new SqlParameter("@ST_Fname", ST_Fname);
+        SqlParameter par3 = new SqlParameter("@ST_Lname", ST_Lname);
+        SqlParameter par4 = new SqlParameter("@ST_Bdate", ST_Bdate);
+        SqlParameter par5 = new SqlParameter("@ST_Phone", ST_Phone);
+        SqlParameter par6 = new SqlParameter("@ST_Salary", ST_Salary);
+        SqlParameter par7 = new SqlParameter("@ST_Street", ST_Street);
+        SqlParameter par8 = new SqlParameter("@ST_City", ST_City);
+        SqlParameter par9 = new SqlParameter("@ST_Country", ST_Country);
+        SqlParameter par10= new SqlParameter("@DP_ID", DP_ID);
+
+        int affected = DAL.RunDML("Insert_Student", new[] { par1, par2,par3, par4, par5, par6, par7, par8, par9, par10 });
+        return affected;
+
+    }
+    public static int DeleteStudent(int ST_ID)
+    {
+        SqlParameter par1 = new SqlParameter("@ST_ID", ST_ID);
+        int affected = DAL.RunDML("Delete_Student",new[] { par1});
+        return affected;
+
+    }
+
+    public static int  UpdateStudent(int ST_ID, string ST_Fname, string ST_Lname, string ST_Bdate, string ST_Phone, decimal ST_Salary, string ST_Street, string ST_City, string ST_Country, int DP_ID)
+    {
+
+        SqlParameter par1 = new SqlParameter("@ST_ID", ST_ID);
+        SqlParameter par2 = new SqlParameter("@ST_Fname", ST_Fname);
+        SqlParameter par3 = new SqlParameter("@ST_Lname", ST_Lname);
+        SqlParameter par4 = new SqlParameter("@ST_Bdate", ST_Bdate);
+        SqlParameter par5 = new SqlParameter("@ST_Phone", ST_Phone);
+        SqlParameter par6 = new SqlParameter("@ ST_Salary", ST_Salary);
+        SqlParameter par7 = new SqlParameter("@ST_Street", ST_Street);
+        SqlParameter par8 = new SqlParameter("@ST_City", ST_City);
+        SqlParameter par9 = new SqlParameter("@ST_Country", ST_Country);
+        SqlParameter par10 = new SqlParameter("@DP_ID", DP_ID);
+
+        int affected = DAL.RunDML("Insert_Student", new[] { par1, par2, par3, par4, par5, par6, par7, par8, par9, par10 });
+        return affected;
+    }
+
 }
